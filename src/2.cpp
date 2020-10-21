8d166082cdd307008b0a02fffe1481a931b406c7
Filip <filipdj98@gmail.com>
2020-10-21 19:19:26 +0200

    Improved 2.cpp

--- src/2.cpp
+++ src/2.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> v;
-#define size 12
 
 class Point{
   public:
@@ -11,25 +10,9 @@ class Point{
     friend bool operator<( const Point& a, const Point& b ) { return a.position<b.position; }
    
 };
-int main(){
-  
-    vector<Point> myVect;
 
-    myVect.push_back(Point(62.0, 58.4,1));
-
-    myVect.push_back(Point(57.5, 56.0,2));
-    myVect.push_back(Point(51.7, 56.0,3));
-    myVect.push_back(Point(67.9,19.6,4));
-    myVect.push_back(Point(57.7, 42.1,5));
-    myVect.push_back(Point(54.2, 29.1,6));
-    myVect.push_back(Point(46.0, 45.1,7));
-    myVect.push_back(Point(34.7, 45.1,8));
-    myVect.push_back(Point(45.7, 25.1,9));
-    myVect.push_back(Point(34.7, 26.4,10));
-    myVect.push_back(Point(28.4, 31.7,11));
-    myVect.push_back(Point(33.4, 60.5,12));
-
-    double** distanceMatrix=new double*[size+1];
+double** initDistanceMatrix(vector<Point> myVect, int size){
+   double** distanceMatrix=new double*[size+1];
 
     for (size_t i = 0; i < size+1; i++)
     {
@@ -42,26 +25,46 @@ int main(){
          distanceMatrix[myVect.at(i).position][myVect.at(j).position]=sqrt(pow(myVect.at(i).x-myVect.at(j).x,2)+pow(myVect.at(i).y-myVect.at(j).y,2));
       }
     }
-      
-    double min=1000000;
+    return distanceMatrix;
+}
+void findMinimum(int size){
+    v.clear();
+
+    
+    //Init vector
+    vector<Point> myVect;
+    myVect.push_back(Point(62.0, 58.4,1));
+    myVect.push_back(Point(57.5, 56.0,2));
+    myVect.push_back(Point(51.7, 56.0,3));
+    myVect.push_back(Point(67.9,19.6,4));
+    myVect.push_back(Point(57.7, 42.1,5));
+    myVect.push_back(Point(54.2, 29.1,6));
+    myVect.push_back(Point(46.0, 45.1,7));
+    myVect.push_back(Point(34.7, 45.1,8));
+    if(size==12){
+      myVect.push_back(Point(45.7, 25.1,9));
+      myVect.push_back(Point(34.7, 26.4,10));
+      myVect.push_back(Point(28.4, 31.7,11));
+      myVect.push_back(Point(33.4, 60.5,12));
+    }
+    double** distanceMatrix=initDistanceMatrix(myVect,size);
+    double min=-1;
     
     auto start = std::chrono::high_resolution_clock::now();
-
     do {
-    double localMin=0;
-    for (size_t i = 0; i < size-1; i++)
-    {
-       // cout<<myVect.at(i).position<<endl;
-        localMin += distanceMatrix[myVect.at(i).position][myVect.at(i+1).position];
-    }
-    if(localMin<min){
-        min=localMin;
-        v.clear();
-      for (size_t i = 0; i < size; i++)
+      double localMin=0;
+      for (size_t i = 0; i < size-1; i++)
       {
-        v.push_back(myVect.at(i).position);
+          localMin += distanceMatrix[myVect.at(i).position][myVect.at(i+1).position];
       }
-    }
+      if(localMin<min||min==-1){
+          min=localMin;
+          v.clear();
+        for (size_t i = 0; i < size; i++)
+        {
+          v.push_back(myVect.at(i).position);
+        }
+      }
   } while (next_permutation(myVect.begin(),myVect.end()));
 
     auto finish = std::chrono::high_resolution_clock::now();
@@ -74,4 +77,15 @@ int main(){
         cout<<v.at(i)<<endl;
       }
 
+
+      //Delete Matrix
+      for (size_t i = 0; i < size; i++)
+      {
+        /* code */
+      }
+      
+}
+int main(){
+
+   findMinimum(8);
 }
